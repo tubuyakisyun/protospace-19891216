e90ae3aae41192114c71d92b31129a2f3e688ad3
tubuyakisyun <lcc3344lwm@gmail.com>
2021-08-28 12:48:41 +0900

    ユーザー管理機能〜sign_in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,13 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  # before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
+  def index
+    @user = User.new
+    @current_user = User.find(params[:name])
+  end
+  def show
+  end
+
 
   private
   def configure_permitted_parameters
