4407174772d14e7d277e14c5385cedf30e2e60c4
tubuyakisyun <lcc3344lwm@gmail.com>
2021-08-28 22:21:07 +0900

    アソシエーションを記述する

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'prototypes#index'
+  resources  :prototypes, only: [:new, :index,:edit, :destroy, :show, :update, :create]
+
 end
   
