df5f3fc43f18bca34bfc1ba5770aca433f1d551d
tubuyakisyun <lcc3344lwm@gmail.com>
2021-08-31 14:26:13 +0900

    メッセージ送信後の条件分岐を設定する

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,13 +1,14 @@
 class PrototypesController < ApplicationController
+  before_action :move_to_index, except: [:index, :show]
+
   def index
-  
+    @prototypes = Prototype.includes(:user)
   end
   def new
     @prototype = Prototype.new
-
   end
   def create
-    Prototype.create(prototype_params)
+    # Prototype.create(prototype_params)
     @prototype = Prototype.new(prototype_params)
     if @prototype.save
       redirect_to root_path(@prototype)
@@ -26,9 +27,17 @@ class PrototypesController < ApplicationController
   end
 
 
+
   private
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+    # ツイート保存時にユーザー情報も追加しよう.merge(user_id: current_user.id)
+    # params.require(:tweet).permit(:name, :image, :text).merge(user_id: current_user.id)
+  end
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
 
 end
