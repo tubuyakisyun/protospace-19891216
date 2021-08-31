ddd965f5c807f97c48fea24f39e732c8064bc451
tubuyakisyun <lcc3344lwm@gmail.com>
2021-08-31 13:03:17 +0900

    User routes 設定する

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'prototypes#index'
-  resources  :prototypes, only: [:new, :index,:edit, :destroy, :show, :update, :create]
-
+  resources  :prototypes, only: [:new, :index, :edit, :destroy, :show, :update, :create]
+  resources :users, only: :show
 end
   
