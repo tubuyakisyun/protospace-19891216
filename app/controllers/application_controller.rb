ea7bec29ec9ea783ff0a8c431b06b5581e3e534d
tubuyakisyun <lcc3344lwm@gmail.com>
2021-08-31 13:00:25 +0900

    Users controller 作成する

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,7 @@
 class ApplicationController < ActionController::Base
-  # before_action :authenticate_user!
+  before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
-  def index
-    @user = User.new
-    @current_user = User.find(params[:name])
-  end
-  def show
-    
-  end
+ 
 
 
   private
