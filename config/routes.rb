20b897557a4a56e01a262d535fc85d0fa7a86a8a
tubuyakisyun <lcc3344lwm@gmail.com>
2021-09-06 15:12:35 +0900

    commentsコントローラーにcreateアクションを設定した（アクション内の処理は書かない）  resourcesを用いて、上記で設定したcreateアクションに対するルーティングをroutes.rbに記述した  rails routesを実行して、ルーティングが正しく設定できていることを確かめた
     commentsコントローラーにcreateアクションを設定した（アクション内の処理は書かない）
     resourcesを用いて、上記で設定したcreateアクションに対するルーティングをroutes.rbに記述した
     rails routesを実行して、ルーティングが正しく設定できていることを確かめた

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'prototypes#index'
-  resources  :prototypes, only: [:new, :index, :edit, :destroy, :show, :update, :create]
+  resources  :prototypes do
+    resources  :comments, only: [:create]
+  end
+
   resources :users, only: :show
 end
   
