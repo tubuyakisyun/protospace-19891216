86214751f78bb18d2de59d1f1a8ea1152ec1cff1
tubuyakisyun <lcc3344lwm@gmail.com>
2021-09-07 23:19:24 +0900

    Update users_controller.rb
     usersコントローラーのshowアクションにインスタンス変数@userを定義した。且つ、Pathパラメータで送信されるID値で、Userモデルの特定のオブジェクトを取得するように記述し、それを@userに代入した

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,13 @@
 class UsersController < ApplicationController
   def show
-    user = User.find(params[:id])
-    @name = user.name
-    @prototypes = user.prototypes 
+    @user = User.find(params[:id])
+    #  usersコントローラーのshowアクションにインスタンス変数@userを定義した。且つ、Pathパラメータで送信されるID値で、Userモデルの特定のオブジェクトを取得するように記述し、それを@userに代入した
+    # @name = user.name
+    # @prototypes = user.prototypes ←こちらだとエラーになってしまった
+    # @name = @user.name              ←こうするとちゃんと機能してる
+    @name = @user.name  
+    @prototypes = @user.prototypes
     
-  end
-  def index
-    @prototypes = Prototype.includes(:user)
-    @user = User.new
-    @current_user = User.find(params[:id])
-    @name = current_user.name
-
   end
 
 end
