50f0439f94dc6c1145e95fc7338b3dc8de6a0b42
tubuyakisyun <86176325+tubuyakisyun@users.noreply.github.com>
2021-09-11 08:27:26 +0900

    Revert "protospace の実装"

--- app/models/prototype.rb
+++
@@ -1,9 +0,0 @@
-class Prototype < ApplicationRecord
-  belongs_to :user
-  has_many :comments
-  has_one_attached :image
-
-  validates :title,            presence: true
-  validates :catch_copy,       presence: true
-  validates :concept,          presence: true
-end
